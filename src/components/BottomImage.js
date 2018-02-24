e94f7450c619de6eb1b8849f5e697386bd0562c7
Juan Camilo Torres <juankmilo9740@hotmail.com>
2018-02-24 17:59:20 -0500

    Se agrega import de StyleSheet faltante en componente BottomImage

--- src/components/BottomImage.js
+++ src/components/BottomImage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 export default class BottomImage extends Component {
     render() {
